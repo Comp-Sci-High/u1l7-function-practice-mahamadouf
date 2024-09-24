d476691df40eece4e0339fbae198d78b148acfed
mahamadouf <123761069+mahamadouf@users.noreply.github.com>
2024-09-24 19:09:32 +0000

    message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Follow the instructions and complete each task.
 // Make sure to run your code using node index.js
 
+
 /*
    ==== Make sure to run your code ====
   use the command 'node index.js' in the terminal
@@ -12,26 +13,44 @@
 // 2. Inside the function, return the result of dividing num1 by num2.
 // 3. Call the `divide` function with the inputs 4 and 5, and log the result.
 
+function divide(num1, num2){
+   return num1/num2
+}
 
-
+divide(4, 5)
+console.log(divide(4, 5))
 // TASK 2: Define a regular function to greet a user.
 // 1. Define a function named `greetUser` that takes one input: name.
 // 2. Inside the function, return a greeting string like "Hello, [name]!"
 // 3. Call the `greetUser` function with your name, and log the result.
 
-
-
+function greetUser(name){
+   return "Hello, "+ name
+}
+greetUser("Mahamadou")
+console.log(greetUser("Mahamadou"))
 // TASK 3: Define an arrow function to subtract two numbers.
 // 1. Define an arrow function named `subtract` that takes two inputs: num1 and num2.
 // 2. Inside the arrow function, return the result of subtracting num2 from num1.
 // 3. Call the `subtract` function with the inputs 10 and 3, and log the result.
 
+const subtract = (num1, num2) => {
+   return num2 - num1
+}
 
-
+subtract(10, 3)
+console.log(subtract(10, 3))
 // TASK 4: Define an arrow function to calculate the square of a number.
 // 1. Define an arrow function named `square` that takes one input: num.
 // 2. Inside the arrow function, return the result of squaring the number (num * num).
 // 3. Call the `square` function with the argument 6, and log the result.
+
+const square = (num) => {
+   return num * num
+}
+
+square(6)
+console.log(square(6))
 
 /*
    ==== Final Commit Instructions ====
@@ -42,3 +61,8 @@
 // BONUS TASK: 
 // Write a new function that within calls any 2 of the above functions and returns the sum or concatenation of their output. 
 // Test your function
+
+
+function above (function1, function2){
+
+}
